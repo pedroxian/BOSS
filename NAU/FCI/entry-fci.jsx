76a43a5c14c7192de6759986dfe89674de9629bf
pedroxian <44950875+pedroxian@users.noreply.github.com>
2020-11-17 19:59:18 +0100

    file comitter

---
+++ NAU/FCI/entry-fci.jsx
@@ -0,0 +1,8 @@
+_RENATAIL:MAGE utem.beton .charger found.synapsy /design.root   ema.tek
+    surger.class.1 -begin .c.utem languages.for -parrot.os /dark.scheme
+
+    {execute.design root.utem c:/format c: -format.b -enter.loud
+        hexe.c ant.room t.proper = font.loop    execute
+        mega.maschine -class.1 touring.f -loud
+            c.formation -beta
+    }class.1 c.1 empty.folder -ful.binaries
